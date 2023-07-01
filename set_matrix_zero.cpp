8cbce841747fb312b851adc9ec30a91bede920c6
Abhijeet <108912315+lets-do-code@users.noreply.github.com>
2023-07-02 01:20:49 +0530

    set_matrix_zero

---
+++ set_matrix_zero.cpp
@@ -0,0 +1,36 @@
+class Solution
+{
+public:
+    void setZeroes(vector<vector<int>> &matrix)
+    {
+
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        unordered_set<int> rows;
+        unordered_set<int> cols;
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (matrix[i][j] == 0)
+                {
+                    rows.insert(i);
+                    cols.insert(j);
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                if (rows.count(i) || cols.count(j))
+                {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+};
